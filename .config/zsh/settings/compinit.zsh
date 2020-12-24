3b815591ef011ccaa33a4d63756a1b9e09816b38
Smitesh Patil <smiteshpatil2010@gmail.com>
2020-12-24 10:08:03 +0530

    Zsh case insensitive and caching
    Make zsh completion case insensitive
    Enable zstyle caching

--- .config/zsh/settings/compinit.zsh
+++ .config/zsh/settings/compinit.zsh
@@ -1,6 +1,14 @@
 # Basic auto/tab complete:
 autoload -Uz compinit
+
 zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*' # Case insensitive matching
+
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.cache
+
 zmodload zsh/complist
+
 compinit -d ~/.cache/zcompdump
+
 _comp_options+=(globdots)		# Include hidden files.
