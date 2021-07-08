4d092e128814dd0a15996a46033471d268909925
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-07-08 21:08:45 +0530

    nvim: telescope breaking changes

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -33,7 +33,7 @@ require("telescope").setup{
 		file_sorter =  require("telescope.sorters").get_fuzzy_file,
 		file_ignore_patterns = { ".out" },
 		generic_sorter =  require("telescope.sorters").get_generic_fuzzy_sorter,
-		shorten_path = true,
+		path_display = { "absolute" },
 		winblend = 0,
 		border = {},
 		borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
