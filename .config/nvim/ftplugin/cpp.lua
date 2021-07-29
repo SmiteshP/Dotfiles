a13802344ff45e8c385621d0d3c710241d6f7a28
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-07-29 22:16:56 +0530

    nvim: CompCoding update
    * remove toggle commands, exec toggles by default now
    * remove extra echo commands and replace them with '\n' wherever
      possible

--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -1,29 +1,25 @@
 function CompCoding_debug()
 	vim.cmd("write")
 	require("toggleterm").exec([[ g++ -std=c++17 -Wall -Wshadow -fsanitize=address -fsanitize=undefined -g -D_GLIBCXX_DEBUG -I ~/CompCoding/PCH_stdc++/debug/ ]] .. vim.fn.expand("%:t")
-	.. [[; if [ $? -eq 0 ]; then echo '\nOutput (debug)\t:' && /usr/bin/time -f '\nTime\t: %e seconds\nMemory\t: %M Kbytes' ./a.out < in; else echo; echo 'Compilation error'; fi ]], 1)
-	require("toggleterm").toggle(1)
+	.. [[; if [ $? -eq 0 ]; then echo '\nOutput (debug)\t:' && /usr/bin/time -f '\nTime\t: %e seconds\nMemory\t: %M Kbytes' ./a.out < in; else echo '\nCompilation error'; fi ]], 1)
 end
 
 function CompCoding_outfile()
 	vim.cmd("write")
 	require("toggleterm").exec([[ g++ -std=c++17 -O2 -I ~/CompCoding/PCH_stdc++/optimized/ ]] .. vim.fn.expand("%:t")
-	.. [[; if [ $? -eq 0 ]; then ./a.out < in > out; else echo; echo 'Compilation error'; fi ]], 1)
-	vim.cmd("echom 'Result written to outfile'")
+	.. [[; if [ $? -eq 0 ]; then ./a.out < in > out; echo '\nResult written to outfile'; else echo '\nCompilation error'; fi ]], 1)
 end
 
 function CompCoding_fast()
 	vim.cmd("write")
 	require("toggleterm").exec([[ g++ -std=c++17 -O2 -I ~/CompCoding/PCH_stdc++/optimized/ ]] .. vim.fn.expand("%:t")
-	.. [[; if [ $? -eq 0 ]; then echo '\nOutput (fast)\t:' && /usr/bin/time -f '\nTime\t: %e seconds\nMemory\t: %M Kbytes' ./a.out < in; else echo; echo 'Compilation error'; fi ]], 1)
-	require("toggleterm").toggle(1)
+	.. [[; if [ $? -eq 0 ]; then echo '\nOutput (fast)\t:' && /usr/bin/time -f '\nTime\t: %e seconds\nMemory\t: %M Kbytes' ./a.out < in; else echo '\nCompilation error'; fi ]], 1)
 end
 
 function CompCoding_interactive()
 	vim.cmd("write")
 	require("toggleterm").exec([[ g++ -std=c++17 -Wall -Wshadow -fsanitize=address -fsanitize=undefined -g -D_GLIBCXX_DEBUG -I ~/CompCoding/PCH_stdc++/debug/ ]] .. vim.fn.expand("%:t")
-	.. [[; if [ $? -eq 0 ]; then echo; ./a.out; else echo; echo 'Compilation error'; fi ]], 1)
-	require("toggleterm").toggle(1)
+	.. [[; if [ $? -eq 0 ]; then echo; ./a.out; else echo '\nCompilation error'; fi ]], 1)
 end
 
 function CompCoding_template()
