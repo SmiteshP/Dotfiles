98f3baa644b3db9dd89da6519ee48523ce9f27eb
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-08-05 09:40:23 +0530

    nvim: onedark update

--- .config/nvim/lua/config/theme/onedark/init.lua
+++ .config/nvim/lua/config/theme/onedark/init.lua
@@ -2,7 +2,7 @@
 vim.cmd("packadd onedark.nvim")
 
 -- Theme
-vim.g.onedark_italic_comments = true
-vim.g.onedark_sidebars = { "NvimTree", "terminal", "packer" }
-
-vim.cmd[[colorscheme onedark]]
+require("onedark").setup {
+	commentStyle = "italic",
+	sidebars = { "NvimTree", "terminal", "packer" }
+}
