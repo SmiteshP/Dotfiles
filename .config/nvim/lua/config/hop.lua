3244ba5156a344ce95b0d1af72666422bcc4ecad
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-08-16 12:54:28 +0530

    nvim: Use Hop like a true vim motion

--- .config/nvim/lua/config/hop.lua
+++ .config/nvim/lua/config/hop.lua
@@ -1,4 +1,4 @@
 require("hop").setup { keys = "asdghkl;weroiutyvnbcmfj" }
 
-vim.api.nvim_set_keymap('n', 's', "<cmd>lua require'hop'.hint_char2()<cr>", {})
-vim.api.nvim_set_keymap('n', 'S', "<cmd>lua require'hop'.hint_words()<cr>", {})
+vim.api.nvim_set_keymap('', 's', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('', 'S', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
