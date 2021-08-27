da15508da96fb7e4c7d1a917286d8444446c1913
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-08-27 22:06:35 +0530

    nvim: switch 's' and 'S' keybinding for hop
    s now is HopWord
    S now is HopChar2

--- .config/nvim/lua/config/hop.lua
+++ .config/nvim/lua/config/hop.lua
@@ -1,8 +1,8 @@
 require("hop").setup { keys = "asdghkl;weroiutyvnbcmfj" }
 
-vim.api.nvim_set_keymap('n', 's', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', 'S', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('x', 's', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('x', 'S', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('o', 's', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('o', 'S', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', 'S', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', 's', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('x', 'S', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('x', 's', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('o', 'S', "<cmd>lua require'hop'.hint_char2()<cr>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('o', 's', "<cmd>lua require'hop'.hint_words()<cr>", { silent = true, noremap = true })
