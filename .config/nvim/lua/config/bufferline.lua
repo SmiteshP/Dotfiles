a0db3e1a0dcb32dd411f469f784c2c6e947b978b
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-08-27 21:53:40 +0530

    nvim: bufferline remove depricated stuff

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -1,8 +1,6 @@
 require("bufferline").setup {
 	options = {
 		numbers = "none",
-		number_style = "", -- buffer_id at index 1, ordinal at index 2
-		mappings = false,
 		indicator_icon = '▎',
 		buffer_close_icon = '',
 		modified_icon = '',
