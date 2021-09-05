38824e21294a5ca006ddc665e7f630a44e54beda
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-09-05 18:47:19 +0530

    nvim: tokyonight update config

--- .config/nvim/lua/config/theme/tokyonight/init.lua
+++ .config/nvim/lua/config/theme/tokyonight/init.lua
@@ -3,6 +3,10 @@ vim.cmd("packadd tokyonight.nvim")
 
 -- Theme
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_comments = true
+vim.g.tokyonight_italic_keywords = false
+vim.g.tokyonight_italic_functions = false
+vim.g.tokyonight_italic_variables = false
 vim.g.tokyonight_sidebars = { "NvimTree", "terminal", "packer" }
 
 vim.cmd[[colorscheme tokyonight]]
