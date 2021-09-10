bfa51c78b147bc9b4d5084224b6cfe67b318800c
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-09-10 18:51:12 +0530

    nvim: gitsigns breaking changes

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -43,5 +43,7 @@ require("gitsigns").setup {
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
-	use_internal_diff = true,  -- If luajit is present
+	diff_opts = {
+		internal = true
+	}
 }
