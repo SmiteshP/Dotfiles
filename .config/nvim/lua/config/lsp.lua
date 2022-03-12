771c79dfd36510a637a7014465711cbb7a8ae121
Smitesh Patil <smiteshpatil2010@gmail.com>
2022-03-12 21:13:50 +0530

    nvim: add rust-analyzer

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -147,3 +147,10 @@ require("lspconfig").sumneko_lua.setup {
 		}
 	}
 }
+
+require("lspconfig").rust_analyzer.setup {
+	cmd = { "/usr/bin/rust-analyzer" },
+	on_attach = common_config.common_on_attach,
+	capabilities = common_config.capabilities,
+	filetypes = { "rust" },
+}
